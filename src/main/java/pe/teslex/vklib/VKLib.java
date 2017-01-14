744136ce9bedd40bc9f162f384a8eae1140803d4
devid <devid.kohut@gmail.com>
2017-01-14 23:12:25 +0200

    wall.post

--- src/main/java/pe/teslex/vklib/VKLib.java
+++ src/main/java/pe/teslex/vklib/VKLib.java
@@ -38,6 +38,29 @@ public class VKLib extends PluginBase {
         return line;
     }
 
+    public static String newWall(String token, int id, String message) {
+        String line = "";
+        String url = "https://api.vk.com/method/"+
+                "wall.post"+
+                "?owner_id="+id+
+                "&message="+message+
+                "&access_token="+token+
+                "&v=5.60"
+                ;
+        try {
+            URL url2 = new URL(url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
+            line = reader.readLine();
+            reader.close();
+
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+        return line;
+    }
+
     @Override
     public void onEnable() {
         this.getLogger().info("Запущен");
