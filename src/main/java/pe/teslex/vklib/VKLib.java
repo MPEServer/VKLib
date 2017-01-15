7b6fc914bc7c2ac05f9f5b6f5f91ecf87ebfca6a
devid <devid.kohut@gmail.com>
2017-01-15 03:31:16 +0200

    Update VKLib.java

--- src/main/java/pe/teslex/vklib/VKLib.java
+++ src/main/java/pe/teslex/vklib/VKLib.java
@@ -43,7 +43,35 @@ public class VKLib extends PluginBase {
         }
         return line;
     }
+    
+    /**
+     * @param token
+     * @param id
+     * @param message
+     * @return
+     */
+    public static String wallPost(String token, int id, String message) {
+        String line = "";
+        String url = "https://api.vk.com/method/"+
+                "wall.post"+
+                "?owner_id="+id+
+                "&message="+message+
+                "&access_token="+token+
+                "&v=5.60"
+                ;
+        try {
+            URL url2 = new URL(url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
+            line = reader.readLine();
+            reader.close();
 
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+        return line;
+    }
 
 
     @Override
